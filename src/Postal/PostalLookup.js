b1e743ecee31c9cb04eb9f43547653be2720bdfd
Ccolina03 <colinaperu2003@gmail.com>
2023-03-30 23:58:00 -0600

    Added foundation of logic to recieve data from the form

--- src/Postal/PostalLookup.js
+++ src/Postal/PostalLookup.js
@@ -3,12 +3,20 @@ import TabBar from "./components/TabBar";
 import Intro from "./components/Intro";
 import Input from "./components/Input";
 
+
 const PostalLookup = () => {
+    const [postal, setPostal]=useState([])
+
+    const handleInput = (input) => {
+        setPostal(input)
+    }
     return(
         <div>
         <TabBar/>
         <Intro/>
-        <Input/>
+        <Input onInput={handleInput}/>
+        { postal & <PostalList postal={postal}/>
+        }
         </div>
     )
 }
---
+++ src/Postal/components/PostalList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PostalList = () => {
+    return(<div>
+        <p>peru</p>
+    </div>)
+}
+
+export default PostalList;
