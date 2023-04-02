b52c908f20ef5e124b60072391e7a4e29217e418
Ccolina03 <colinaperu2003@gmail.com>
2023-04-02 16:55:51 -0600

    Finish Modal with POST

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -17,15 +17,16 @@ const Home = () => {
     }
 
     return (
-        <div className="flex justify-center">
+        <div>
             <TabBar/>
             <Intro/>
             <Input onHandleInput={HandleInput}/>
-            <button className ="text-white bg-gradient-to-r from-pink-200 via-pink-500 to-pink-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={() => setShowModal(true)}>Add Post</button>
+            <div className="flex justify-center">
+            <button className ="flex justify-center mt-20 text-white bg-gradient-to-r from-pink-200 via-pink-500 to-pink-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-lg px-5 py-2.5 mr-2 mb-2" onClick={() => setShowModal(true)}>Add Post</button>
+            </div>
             <AddPostModal showModal={showModal} setShowModal={setShowModal} />
             {(search !== "") ? <PostsList onSearch={search}/> : <PostsList />}
         </div>
     );
 }
-
 export default Home;
